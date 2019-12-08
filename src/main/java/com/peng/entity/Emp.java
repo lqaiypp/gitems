f10fa163c68e86a75b056038d0481c1963ae811a
lq <598574975@qq.com>
2019-12-08 10:57:50 +0800

    添加员工对象

--- src/main/java/com/peng/entity/Emp.java
+++ src/main/java/com/peng/entity/Emp.java
@@ -1,11 +1,20 @@
 package com.peng.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 import java.util.Date;
 
 /**
  * Created by liuqian on 2019/12/8.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
 public class Emp implements Serializable {
     private String id;
     private String name;
