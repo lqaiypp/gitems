718f5754c4ccc8ae78e5a2dc360a1f3b607e2913
lq <598574975@qq.com>
2019-12-08 12:47:24 +0800

    修改最初配置

--- src/main/java/com/peng/GitemsApplication.java
+++ src/main/java/com/peng/GitemsApplication.java
@@ -2,8 +2,9 @@ package com.peng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
+@MapperScan("com.peng.dao")
 public class GitemsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/peng/entity/Emp.java
+++ src/main/java/com/peng/entity/Emp.java
@@ -1,10 +1,13 @@
 package com.peng.entity;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -16,9 +19,12 @@ import java.util.Date;
 @NoArgsConstructor
 @Accessors(chain = true)
 public class Emp implements Serializable {
+    @Id
     private String id;
     private String name;
     private Double salary;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JSONField(format = "yyyy-MM-dd")
     private Date bir;
 
 }
