c0c75fd4b0b8678e9f69e182409dea121633db2e
lq <598574975@qq.com>
2019-12-08 10:54:38 +0800

    添加员工对象

---
+++ src/main/java/com/peng/entity/Emp.java
@@ -0,0 +1,15 @@
+package com.peng.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by liuqian on 2019/12/8.
+ */
+public class Emp implements Serializable {
+    private String id;
+    private String name;
+    private Double salary;
+    private Date bir;
+
+}
