97f314d3325959f21745b68d0aa213d59ea52e21
lq <598574975@qq.com>
2019-12-08 10:22:08 +0800

    这是初始化配置

---
+++ src/main/java/com/peng/GitemsApplication.java
@@ -0,0 +1,13 @@
+package com.peng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitemsApplication.class, args);
+    }
+
+}
